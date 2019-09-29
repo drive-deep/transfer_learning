a1c6e15d898007b75d43ba18bd45fcc255253bb8
drive-deep <32527844+drive-deep@users.noreply.github.com>
2019-09-30 03:28:10 +0530

    Update assignment_6.py

--- assignment_6.py
+++ assignment_6.py
@@ -188,7 +188,7 @@ optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
 
 model_conv = train_model(model_conv, criterion, optimizer_conv,
-                         exp_lr_scheduler, num_epochs=1)
+                         exp_lr_scheduler, num_epochs=24)
 
 visualize_model(model_conv)
 
